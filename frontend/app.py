071229a04ad2430178930dcee69c1237e31cedea
y <yqwx01z@gmail.com>
2025-03-03 01:51:33 +0100

    commit

--- frontend/app.py
+++ frontend/app.py
@@ -466,7 +466,7 @@ with gr.Blocks() as app:
     btn = gr.Button("Search")
     out = gr.Textbox(visible=False)
 
-    model_dropdown = gr.Dropdown(choices=['facebook/opt-125m'], interactive=True, show_label=False, visible=False)
+    model_dropdown = gr.Dropdown(choices=[''], interactive=True, show_label=False, visible=False)
 
     with gr.Row():
         selected_model_id = gr.Textbox(label="id",visible=False)
@@ -684,7 +684,9 @@ with gr.Blocks() as app:
     timer_dl.tick(get_download_speed, outputs=timer_dl_box)
 
     btn_dl.click(lambda: gr.update(label="Starting download ...",visible=True), None, create_response).then(lambda: gr.update(visible=True), None, timer_dl_box).then(lambda: gr.Timer(active=True), None, timer_dl).then(download_from_hf_hub, model_dropdown, create_response).then(lambda: gr.Timer(active=False), None, timer_dl).then(lambda: gr.update(label="Download finished!"), None, create_response).then(lambda: gr.update(visible=True), None, btn_interface)
-    btn_update.click(docker_api, inputs=["update",[model_dropdown]], outputs=create_response)
+    
+    action_textbox = gr.Textbox(value="update", visible=True)
+    btn_update.click(docker_api, inputs=[["update"],[model_dropdown]], outputs=create_response)
     # btn_service.click(docker_api, inputs=["service",model_dropdown], outputs=create_response)
     # prompt_btn.click(docker_api, inputs=["generate",model_dropdown,"auto",prompt_in,0.8], outputs=prompt_out)
 
